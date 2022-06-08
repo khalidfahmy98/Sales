093afd0e3521bc11615f28b7778bdd4205e8952d
khalid <khalidahmed0551863699@gmail.com>
2022-06-08 16:27:11 +0200

    created customers base controller

---
+++ Sales/Sales/Controllers/CustomersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Entity;
+using BLL;
+using Sales.Common;
+
+namespace Sales.Controllers
+{
+    public class CustomersController : Controller
+    {
+        [IsLogged]
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Create Customer";
+            return View();
+        }
+        [IsLogged]
+        public ActionResult Operations()
+        {
+            ViewBag.Title = " Customers Operations";
+            return View();
+        }
+
+    }
+}
