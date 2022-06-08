29c13eea5105847c57c286cccaad2644346670a7
khalid <khalidahmed0551863699@gmail.com>
2022-06-08 20:00:19 +0200

    created areas BLL class

--- Sales/BLL/AreasBLL.cs
+++ Sales/BLL/AreasBLL.cs
@@ -10,5 +10,17 @@ namespace BLL
 {
     public class AreasBLL
     {
+        public static IQueryable<Areas> List() => DataService.Areas.List();
+
+        public static Areas Get(int Id) => DataService.Areas.Get(Id);
+
+        public static int Add(Areas Model) => DataService.Areas.Create(Model).Id;
+
+        public static int Count() => List().Count();
+
+        public static bool Edit(Areas Model) => DataService.Areas.Update(Model);
+
+        public static bool Delete(int Id) => DataService.Areas.Delete(Id);
+
     }
 }
