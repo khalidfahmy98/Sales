568d3a6cca32039b401689a8f486234426e18f0d
khalid <khalidahmed0551863699@gmail.com>
2022-06-09 00:23:51 +0200

    created areas script s

---
+++ Sales/Sales/Scripts/Areas/Add.js
@@ -0,0 +1,27 @@
+﻿function Add() {
+    var name = $("#name").val(),
+        comment = $("#comment").val(),
+        model = { Area: name, Comment: comment};
+    if (name !== "" && comment !== "") {
+        $.ajax({
+            url: "/Areas/Create",
+            type: 'POST',
+            data: JSON.stringify({ model: model }),
+            async: false,
+            contentType: 'application/json; charset=utf-8',
+            cache: false,
+            processData: false,
+            success: function (data, textStatus, jqXHR) {
+                if (data == "success") {
+                    ReloadView();
+                    $("#errorHandler").removeClass("hide").addClass("alert-success").removeClass("alert-danger").text("Created New Customer Areas Successfully .. ! ");
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert(errorThrown);
+            }
+        });
+    } else {
+        $("#errorHandler").removeClass("hide").addClass("alert-danger").text("Cant Register Empty Values .");
+    }
+}
