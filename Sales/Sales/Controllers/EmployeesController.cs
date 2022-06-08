2ee33be72e22128aadafcb22f0807c95a22fe28d
khalid <khalidahmed0551863699@gmail.com>
2022-06-08 16:37:56 +0200

    created employee base controller

---
+++ Sales/Sales/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BLL;
+using Entity;
+using Sales.Common;
+
+namespace Sales.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        [IsLogged]
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Create Employee";
+            return View();
+        }
+        [IsLogged]
+        public ActionResult Operations()
+        {
+            ViewBag.Title = " Employee Operations";
+            return View();
+        }
+
+    }
+}
