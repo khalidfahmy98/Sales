1c8d6fe715dd2aa10a8d364376861cd7b8c7ca1f
khalid <khalidahmed0551863699@gmail.com>
2022-06-08 16:42:20 +0200

    created products base controller

---
+++ Sales/Sales/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BLL;
+using Entity;
+using Sales.Common;
+
+namespace Sales.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [IsLogged]
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Products Operations";
+            return View();
+        }
+    }
+}
