2b65e5376b53b622970adc386b74e91d7ced6ade
khalid <khalidahmed0551863699@gmail.com>
2022-06-09 00:29:36 +0200

    specials cruds crearted successfully

--- Sales/Sales/Controllers/SpecialsController.cs
+++ Sales/Sales/Controllers/SpecialsController.cs
@@ -17,5 +17,31 @@ namespace Sales.Controllers
             ViewBag.Title = "Specials Operations";
             return View();
         }
+        [IsLogged]
+        public ActionResult DataView()
+        {
+            List<Specials> types = SpecialsBLL.List().ToList();
+            return View(types);
+        }
+        [IsLogged]
+        public JsonResult Create(Specials model)
+        {
+            if (SpecialsBLL.Add(model) != 0)
+            {
+                return Json("success", JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { error = "error", msg = "Incorrect Information .. ! " }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [IsLogged]
+        public JsonResult Del(int Id)
+        {
+            SpecialsBLL.Delete(Id);
+            return Json("success", JsonRequestBehavior.AllowGet);
+        }
+
     }
+
 }
---
+++ Sales/Sales/Scripts/Specials/Operations.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+    ReloadView();
+});
+function Del(id, ele) {
+    $.ajax({
+        url: "/Specials/Del",
+        type: 'POST',
+        data: JSON.stringify({ Id: id }),
+        async: false,
+        contentType: 'application/json; charset=utf-8',
+        cache: false,
+        processData: false,
+        success: function (data, textStatus, jqXHR) {
+            if (data == "success") {
+                ReloadView();
+                $("#errorHandler").removeClass("hide").addClass("alert-success").removeClass("alert-danger").text("Deleted Customer Specials Successfully");
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+}
+function ReloadView() {
+    $.ajax({
+        url: "/Specials/DataView",
+        type: 'GET',
+        async: false,
+        contentType: 'charset=utf-8',
+        cache: false,
+        processData: false,
+        success: function (data, textStatus, jqXHR) {
+            $("#View").html(data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+}
