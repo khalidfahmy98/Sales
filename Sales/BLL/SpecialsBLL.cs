d0e50bb4e325a477013c851c48bad20c43ac0ee2
khalid <khalidahmed0551863699@gmail.com>
2022-06-08 20:00:50 +0200

    created specials bll class

--- Sales/BLL/SpecialsBLL.cs
+++ Sales/BLL/SpecialsBLL.cs
@@ -10,5 +10,16 @@ namespace BLL
 {
     public class SpecialsBLL
     {
+        public static IQueryable<Specials> List() => DataService.Specials.List();
+
+        public static Specials Get(int Id) => DataService.Specials.Get(Id);
+
+        public static int Add(Specials Model) => DataService.Specials.Create(Model).Id;
+
+        public static int Count() => List().Count();
+
+        public static bool Edit(Specials Model) => DataService.Specials.Update(Model);
+
+        public static bool Delete(int Id) => DataService.Specials.Delete(Id);
     }
 }
