210eeddc7f864f194e6eb01591a99414a5fc1cff
khalid <khalidahmed0551863699@gmail.com>
2022-06-08 17:57:31 +0200

    created products BLL class with functions

--- Sales/BLL/ProductsBLL.cs
+++ Sales/BLL/ProductsBLL.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entity;
+using DAL;
 
 namespace BLL
 {
     public class ProductsBLL
     {
-        public static IQueryable<Categories> List() => DataService.Categories.List();
+        public static IQueryable<Products> List() => DataService.Products.List();
 
-        public static Categories Get(int Id) => DataService.Categories.Get(Id);
+        public static Products Get(int Id) => DataService.Products.Get(Id);
 
-        public static int Add(Categories Model) => DataService.Categories.Create(Model).Id;
+        public static int Add(Products Model) => DataService.Products.Create(Model).Id;
 
         public static int Count() => List().Count();
 
-        public static bool Edit(Categories Model) => DataService.Categories.Update(Model);
+        public static bool Edit(Products Model) => DataService.Products.Update(Model);
 
-        public static bool Delete(int Id) => DataService.Categories.Delete(Id);
+        public static bool Delete(int Id) => DataService.Products.Delete(Id);
 
     }
 }
