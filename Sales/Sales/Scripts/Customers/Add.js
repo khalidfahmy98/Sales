2c5ead4838ebe0cb45051c9501539bb225b444d8
khalid <khalidahmed0551863699@gmail.com>
2022-06-09 01:08:50 +0200

    created customer insert and operations

--- Sales/Sales/Scripts/Customers/Add.js
+++ Sales/Sales/Scripts/Customers/Add.js
@@ -1,1 +1,41 @@
-﻿
+﻿function Add() {
+    var fullname = $("#fullname").val(),
+        phone = $("#phone").val(),
+        mobilep = $("#mobilep").val(),
+        mobiles = $("#mobiles").val(),
+        pharmap = $("#pharmap").val(),
+        pharmas = $("#pharmas").val(),
+        address = $("#address").val(),
+        area = $("#area").val(),
+        type = $("#type").val(),
+        comment = $("#comment").val(),
+        special = $("#special").val(),
+        model = {
+            Name: fullname, Phone: phone, MobileP: mobilep
+            , MobileS: mobiles, Address: address, NearestPharmacyP: pharmap
+            , NearestPharmacyS: pharmas, TypeId: type, SpecialId: special , Comment : comment , AreaId : area
+        };
+    if (fullname !== "" && username !== "" && password !== "") {
+        $.ajax({
+            url: "/Customers/Create",
+            type: 'POST',
+            data: JSON.stringify({ model: model }),
+            async: false,
+            contentType: 'application/json; charset=utf-8',
+            cache: false,
+            processData: false,
+            success: function (data, textStatus, jqXHR) {
+                if (data == "success") {
+                    $("#errorHandler").removeClass("hide").addClass("alert-success").removeClass("alert-danger").text("Created New Customer Account Successfully ..  ! ");
+                } else {
+                    $("#errorHandler").removeClass("hide").addClass("alert-danger").text(data.msg);
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert(errorThrown);
+            }
+        });
+    } else {
+        $("#errorHandler").removeClass("hide").addClass("alert-danger").text("Cant Register Empty Values .");
+    }
+}
--- Sales/Sales/Scripts/Customers/Operations.js
+++ Sales/Sales/Scripts/Customers/Operations.js
@@ -1,1 +1,21 @@
-﻿
+﻿function Del(id, ele) {
+    $.ajax({
+        url: "/Customers/Del",
+        type: 'POST',
+        data: JSON.stringify({ Id: id }),
+        async: false,
+        contentType: 'application/json; charset=utf-8',
+        cache: false,
+        processData: false,
+        success: function (data, textStatus, jqXHR) {
+            if (data == "success") {
+                $(ele).parent("td").parent("tr").remove();
+                $("#errorHandler").removeClass("hide").addClass("alert-success").removeClass("alert-danger").text("Customer Deleted Successfully");
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+}
+
