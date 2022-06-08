2f9284c6e46c5cd2e982a532fc2478c559c7e2e4
khalid <khalidahmed0551863699@gmail.com>
2022-06-08 18:48:06 +0200

    product Crud operations created and implemented successfully

--- Sales/Sales/Scripts/Products/Add.js
+++ Sales/Sales/Scripts/Products/Add.js
@@ -1,1 +1,28 @@
-﻿
+﻿function Add() {
+    var fullname = $("#fullname").val(),
+        comment = $("#comment").val(),
+        model = { Name: fullname, Comment: comment};
+    if (fullname !== "" && comment !== "" ) {
+        $.ajax({
+            url: "/Products/Create",
+            type: 'POST',
+            data: JSON.stringify({ model: model }),
+            async: false,
+            contentType: 'application/json; charset=utf-8',
+            cache: false,
+            processData: false,
+            success: function (data, textStatus, jqXHR) {
+                if (data == "success") {
+                    $("#errorHandler").removeClass("hide").addClass("alert-success").removeClass("alert-danger").text("Created New Product Successfully ..  ! ");
+                } else {
+                    $("#errorHandler").removeClass("hide").addClass("alert-danger").text(data.msg);
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert(errorThrown);
+            }
+        });
+    } else {
+        $("#errorHandler").removeClass("hide").addClass("alert-danger").text("Cant Register Empty Values .");
+    }
+}
