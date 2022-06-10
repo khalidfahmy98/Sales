e2065bf2d4e0ab7cecb431cc02e5d86b39612dec
khalid <khalidahmed0551863699@gmail.com>
2022-06-10 16:56:54 +0200

    linked project to online server to deploy

--- Sales/Entity/entity.Context.cs
+++ Sales/Entity/entity.Context.cs
@@ -35,7 +35,6 @@ namespace Entity
         public virtual DbSet<Products> Products { get; set; }
         public virtual DbSet<Scheduale> Scheduale { get; set; }
         public virtual DbSet<Specials> Specials { get; set; }
-        public virtual DbSet<sysdiagrams> sysdiagrams { get; set; }
         public virtual DbSet<Types> Types { get; set; }
     }
 }
