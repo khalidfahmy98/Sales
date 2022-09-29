10f971275c0ea6ad9850895dc4183c3821645a39
khalid <khalidahmed0551863699@gmail.com>
2022-09-29 13:41:16 +0200

    Reporting Controller and view strcture created

---
+++ Sales/Sales/Controllers/ReportingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Entity;
+using BLL;
+using Sales.Common;
+
+namespace Sales.Controllers
+{
+    public class ReportingController : Controller
+    {
+        // GET: Reporting
+        [IsLogged]
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Day Report ";
+            return View();
+        }
+    }
+}
