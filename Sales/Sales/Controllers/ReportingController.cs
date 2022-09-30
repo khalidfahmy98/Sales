6f055e86e137b0949b452dc89cd7f392c4722c01
khalid <khalidahmed0551863699@gmail.com>
2022-09-30 15:03:42 +0200

    Reporting Dataview script created

--- Sales/Sales/Controllers/ReportingController.cs
+++ Sales/Sales/Controllers/ReportingController.cs
@@ -15,6 +15,11 @@ namespace Sales.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Day Report ";
+            return View();
+        }
+        [IsLogged]
+        public ActionResult Dataview()
+        {
 
             return View();
         }
--- Sales/Sales/Scripts/Reporting/Operations.js
+++ Sales/Sales/Scripts/Reporting/Operations.js
@@ -1,3 +1,22 @@
 ﻿$(document).ready(function () {
-
+    ReloadView();
 });
+function ReloadView() {
+    var date = $("#monthTracker").text().split("/"),
+        month = date[1];
+    $.ajax({
+        url: "/Reporting/Dataview?Month=" + month,
+        data: JSON.stringify({ Month: month }),
+        type: 'GET',
+        async: false,
+        contentType: 'charset=utf-8',
+        cache: false,
+        processData: false,
+        success: function (data, textStatus, jqXHR) {
+            $("#View").html(data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+}
